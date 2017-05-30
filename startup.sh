f3e3a01621e4132d9921ada580ef4b60f67357c0
Gregory Siems <gsiems@gmail.com>
2017-05-29 19:09:35 -0500

    Add checks to see if scripts are already running. Added check to see if data directory is already mounted.

--- startup.sh
+++ startup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-sh /home/pi/init_ttyAMA0.sh
+# Ensure that things are running and that other things are mounted...
+# Note make sure to background the running things.
 
-# Note make sure to background these:
-python /home/pi/watch_4_shutdown.py &
+pgrep watch_4_shutdown.py || /home/pi/watch_4_shutdown.py &
 
-python /home/pi/take_pics.py > /home/pi/take_pics.log &
+[ -d /home/pi/data ] || mkdir -p /home/pi/data
+mounted=`mount | grep /home/pi/data`
+[ "$mounted" ] || mount /dev/sda1 /home/pi/data
 
+pgrep take_pics.py || /home/pi/take_pics.py &
